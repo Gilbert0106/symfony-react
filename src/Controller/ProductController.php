dd87f66b1705415318e6b0470d5ca44eb2a3cac9
Giblert Brandow <simon@0100.se>
2023-12-06 11:16:21 +0100

    Made use of the EntityValueResolver

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -24,13 +24,12 @@ class ProductController extends AbstractController
     }
 
     #[Route('/product/{id}', name: 'product_show')]
-    public function show(EntityManagerInterface $entityManager, int $id): Response
+    public function show(Product $product): Response
     {
-        $product = $entityManager->getRepository(Product::class)->find($id);
 
         if (!$product) {
             throw $this->createNotFoundException(
-                'No product found for id '.$id
+                'No product found for id'
             );
         }
 
